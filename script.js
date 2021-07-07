02a666b03a3f5d2d94370a70a6ffad155354a1e7
Fernando-Escritorio <fernando.takashi12@gmail.com>
2021-07-07 19:08:14 -0300

    backToInitialColor

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 const getRandomColor = () => {
     const letters = '0123456789ABCDEF';
     let color = '#';
-    for (const i = 0; i < 6; i++) {
+    for (let i = 0; i < 6; i++) {
         color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
 }
 
-const setColor = () => document.body.style.backgroundColor = getRandomColor()
+const initialColor = () => {
+    document.body.style.backgroundColor = getRandomColor();
+}
+
+const setColor = () => document.body.style.backgroundColor = getRandomColor();
 
 const button = document.querySelector("#color-changer");
 
+initialColor();
 button.addEventListener('click', setColor);
+let initial = document.querySelector('.color').style.backgroundColor;
+const backInitial = () => setTimeout( () => document.body.style.backgroundColor = initial, 5000)
+button.addEventListener('click', backInitial);
